6547e6c5dcc122e42cc51f9a8509bafa91dd4ba4
giddy <oviemunooboro@gmail.com>
2024-11-25 15:50:08 +0100

    common

--- src/CandidateGeneration/constants/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import sys
-import os
-
-
-CANDID_GEN_CONFIG_FILE_PATH = Path('src\CandidateGeneration\config\configuration.py')
-SCHEMA_FILE_APTH = Path('schema.yaml')
-PARAMS_FILE_PATH = Path('params.yaml')
--- src/utils/commons.py
+++ src/utils/commons.py
@@ -1,7 +1,5 @@
 import os
 # Exception for handling Box value errors
-import boto3.resources
-import boto3.session
 from box.exceptions import BoxValueError
 import sys
 import yaml
@@ -16,84 +14,8 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any,List
 import zipfile
-import boto3
-from botocore.exceptions import ClientError
-from botocore.client import Config
-from dotenv import load_dotenv
-from src.logger import logging
-
-load_dotenv(dotenv_path='.env',override=True)
-
-
-class S3Client:
-    def __init__(self):
-        self.s3client = boto3.client(
-            's3',
-            endpoint_url = os.getenv('ENDPOINT_URL'),
-            aws_access_key_id = os.getenv('MINIO_ACCESS_KEY'),
-            aws_secret_access_key = os.getenv('MINIO_SECRET_KEY'),
-            config = Config(signature_version='s3v4')
-        )
+from glob import glob
         
-    
-    def create_bucket(self,bucketname):
-        '''
-        Create an S3 bucket 
-        
-        Args:
-            - bucketname: Name of the bucket
-            '''
-        try:
-            # create bucket
-            self.s3client.create_bucket(Bucket='new')
-            logging.info(f'Create a Minio S3 bucket with name: {bucketname}')
-        except ClientError as e:
-            logging.info(f'Could not create bucket {bucketname} because {e}')
-            return False
-        return True
-
-    def upload_file(self,filename,bucket,object_name=None):
-        ''''
-        upload files into S3 bucket 
-        
-        Args:
-            - flename: name of the file to upload
-            - Object_name :  name of the s3 bucket, if None, filename is used
-            - bucket : name of the bucket to upload file
-            '''
-        
-        if object_name == None:
-            object_name = os.path.basename(filename)
-
-        try:
-            self.s3client.upload_file(filename,
-                                      bucket,
-                                      object_name,
-                                      )
-            logging.info(f'Uploaded {filename} successfully as {object_name} into {bucket} S3 bucket')
-
-        except ClientError as e:
-            logging.info(f'could not upload file into bucket because : {e}')
-
-    def download_file(self,bucket,object_name,filename):
-        '''
-        Function to download files from s3 bucket
-        
-        Args:
-            - bucket : Name of the bucket
-            - object_name: name of the object
-            - filename : file name to save the object'''
-        
-        try:
-            self.s3client.download_file(bucket,object_name,filename)
-            logging.info(f'successfully downloaded file {object_name} from S3 bucket to {filename}')
-        
-        except ClientError as e:
-            logging.info(f'Unable to download files from S3 because : {e}')
-
-        
-
-
 
 
 @ensure_annotations
@@ -114,6 +36,7 @@ def read_yaml(filepath:str) -> ConfigBox :
             # load the content from yaml file
             content = yaml.safe_load(yaml_obj)
             logging.info(f'Yaml file:  {filepath} loaded suscessfully')
+            return ConfigBox(content)
 
     except BoxValueError:
         raise ValueError('YAML file is empty')
@@ -260,15 +183,22 @@ def unzip_files(zip_file: str, output_dir: str) -> List[str]:
     extracted_files = []
     try:
         # Make sure output directory exists
-        os.makedirs(output_dir, exist_ok=True)
+        create_directories([output_dir])
+       # os.makedirs(output_dir, exist_ok=True)
         
+        logging.info(F'Unzipping data frrom {zip_file} to load into {output_dir}')
         # Open and extract the zip file
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(output_dir)
+
         
         logging.info(f"Files extracted successfully to {output_dir}")
     except Exception as e:
         logging.info(f"Error during extraction: {e}")
+
+    
+
+
 
 
 
