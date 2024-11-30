feca5fc4fa8537765d35082563bb8a9c6af6b0d8
giddy <oviemunooboro@gmail.com>
2024-11-30 21:48:44 +0000

    azure blob storage

--- src/cloud_storage/azure_blob_storage.py
+++ src/cloud_storage/azure_blob_storage.py
@@ -2,6 +2,7 @@ from azureml.core import Workspace , Datastore ,Dataset
 from src.logger import logging
 import os
 from src.utils.commons import unzip_files
+from glob import glob
 
 
 class AzureDatastore:
@@ -19,33 +20,51 @@ class AzureDatastore:
             logging.info(f'Error coccure while setting up workspace : {e}')
 
 
-    def load_local_data_to_Azure_datastore(self,local_path,target_path,registered_name) :
+
+
+    def load_local_data_to_Azure_datastore(self,src_dir,target_path,registered_name) :
 
         '''
         This function is to load the data from local machine to azure datastore
         
         Args:
-            - local_path : path to the data in local machine
-            - target_path :  distination of the file in the workspace
+            - src_dir : path to the data in local machine
+            - target_path :  distination of the file in the workspace blob storage
             - name : how to register the data in workspace
             '''
         try :
 
             logging.info('Uploading data from local machine into blob storage using datastore')
-            self.datastore.upload_files(
-                files=[local_path],
+            self.datastore.upload(
+                src_dir= src_dir,
                 target_path=target_path,
                 overwrite=True
             )
 
+            logging.info('Files uploaded into blob store sucessesfully')
+
             # registering the the dataset into workspace
 
             logging.info('Registering dataset into workspace')
-            dataset = Dataset.File.from_files(path=(self.datastore,local_path))
-            dataset.register(Workspace=Workspace,name=registered_name)
+            # create file dataset from all csv files in the directory
+            file_dataset_path = os.path.join(target_path, '*.csv')
+            
+            logging.info(f'File path for dataset: {file_dataset_path}')
+
+            dataset = Dataset.File.from_files(path=(self.datastore,file_dataset_path))
+            logging.info(f'Registering dataset with name: {registered_name}')
+            dataset.register(workspace=self.workspace,name=registered_name)
+
+            # Check if the dataset exists in the workspace
+            datasets = self.workspace.datasets
+            if registered_name in datasets:
+                logging.info(f'Dataset {registered_name} is registered successfully.')
+            else:
+                logging.error(f'Dataset {registered_name} is not registered.')
+
 
         except Exception as e:
-            logging.info('Error occured : could not upload data to blob storage')
+            logging.info(f'Error occured : could not upload data to blob storage: {e}')
 
     
     def stream_from_datastore(self,registered_name):
@@ -56,14 +75,13 @@ class AzureDatastore:
             - name : name of how the dataset wat registed in the workspace
             
         outs:
-            pandas dataframe'''
+            list of all the filepath of each data streams'''
+        
+        if registered_name in self.workspace.datasets:
 
-        dataset = Dataset.get_by_name(Workspace,name=registered_name)
-        # unzip the zip folder
-
-        unzip_files(dataset,)
-
-        return dataset
+            dataset = Dataset.get_by_name(workspace=self.workspace,name=registered_name)
+        
+            return dataset.to_path()
         
 
 
